ae52964afd0d5acd0bdcfb81717d672eaba4a3a3
prabaltomar11 <prabalpratapsinghtomar11@gmail.com>
2026-08-07 11:17:52 +0530

    feat: add voice recognition and runtime input mode selection

--- main.py
+++ main.py
@@ -2,22 +2,44 @@ from core.router import route
 from voice.listener import listen
 from utils import logger
 from utils.parser import extract_command
-from utils.history import add_history
+import utils.history
+from voice.listener import listen
 
+
+def choose_input_mode():
+        while True:
+
+            mode = input("Choose input mode:\n 1. Text\n 2. Voice\n").strip().lower()
+            if mode in ["1", "text"]:
+                return "text"
+            
+            elif mode in ["2", "voice"]:
+                return "voice"
+            
+            else:
+                logger.error("Invalid input mode. Please choose 'text' or 'voice'.")
 
 def main():
-    logger.info("Welcome to Omnix")
+    logger.info(" Hello Sir.\nWelcome to Omnix")
+    mode = choose_input_mode()
 
-    while user_input := listen():
-        if user_input.lower().strip() in ["exit", "quit", "bye"]:
-            logger.info("Exiting Omnix. Goodbye!")
+    while True:
+         user_input = listen(mode)
+
+         if user_input is None:
+            logger.error("No input received. Please try again.")
+            continue
+
+         if user_input.lower().strip() in ["exit", "quit", "bye"]:
+            logger.success("Exiting Omnix. Goodbye Sir!")
             break
 
-        add_history(user_input)
+       
+         utils.history.add_history(user_input)
 
-        action, app_name, website_name = extract_command(user_input)
+         action, app_name, website_name = extract_command(user_input)
 
-        route(action, app_name, website_name)
+         route(action, app_name, website_name)
 
 
 if __name__ == "__main__":
